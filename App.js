b005aa31183b80d7df29f0564f7a75ecdf6d6a16
Minsung Kim <kim_minsung@@outlook.com>
2023-01-03 22:50:51 +0900

    TodoList 외형 완료

--- App.js
+++ App.js
@@ -5,41 +5,90 @@ import { Feather } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
 
 export default function App() {
-  const onPress = () => {
-    console.log('button pressed');
+  //Category State 생성 
+  const [todos, setTodos] = React.useState([]);
+  const [category, setCategory] = React.useState('JavaScript'); // JavaScript, React, Coding Test
+  const [text, setText] = React.useState('');
+  //Data Structure
+  const newTodo = {
+    id: Date.now,
+    text,
+    completed: false,
+    editMode: false,
+    category,
+  }
+
+  // addTodo가 State 변경시키게 생성 
+  const addTodo = () => {
+    setTodos(prev => [...prev, ...newTodo]);
+    setText('');
+  }
+
+
+
+  // setDone이 State 변경시키게 생성
+  const setDone = (id) => {
+    // 1. id를 매개변수로 받는다. 
+    // 2. todos를 복사한다.
+    // 3. id와 일치하는 todo의 completed를 변경한다.
+    // 4. setTodos를 호출한다.
+    const newTodos = [...todos];
+    const idx = newTodos.findIndex((todo) => todo.id === id);
+    newTodos[idx].completed = !newTodos[idx].completed;
+    setTodos(newTodos);
   };
 
 
-  const [text, onChangeText] = React.useState("Useless Text");
+
 
   return (
     // 버튼 세개 생성
     <SafeAreaView style={styles.container}>
+      <StatusBar style="auto" />
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button} onPress={onPress}><Text>JavaScript</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={onPress}><Text>React</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={onPress}><Text>Coding Test</Text></TouchableOpacity>
+        <TouchableOpacity onPress={() => setCategory("JavaScript")} style={{
+          ...styles.tab,
+          backgroundColor: category === "JavaScript" ? "#0FBCF9" : "grey",
+        }}><Text>JavaScript</Text></TouchableOpacity>
+        <TouchableOpacity onPress={() => setCategory("React")} style={{
+          ...styles.tab,
+          backgroundColor: category === "React" ? "#0FBCF9" : "grey",
+        }}><Text>React</Text></TouchableOpacity>
+        <TouchableOpacity onPress={() => setCategory("CodingTest")} style={{
+          ...styles.tab,
+          backgroundColor: category === "CodingTest" ? "#0FBCF9" : "grey",
+        }}><Text>Coding Test</Text></TouchableOpacity>
       </View>
 
       <View style={styles.lineDivider}></View>
-
-      <TextInput
-        style={styles.todoTextInput}
-        onChangeText={onChangeText}
-        value={text}
-      />
+      <TextInput onSubmitEditing={addTodo} style={styles.todoTextInput} placeholder="할 일을 입력하세요"></TextInput>
       <View style={styles.lineDivider}></View>
 
       <ScrollView style={styles.listContainer}>
 
-        <View style={styles.eachTodoCard}>
-          <View style={styles.todoCard}><Text style={styles.todoText}>Todo List</Text></View>
-          <View style={styles.fuctionBox}>
-            <AntDesign name="checkcircleo" size={30} color="black" />
-            <Feather name="edit" size={30} color="black" />
-            <AntDesign name="delete" size={30} color="black" />
-          </View>
-        </View>
+
+        {/* map 생성 후 addTodo 해야함 */}
+
+        {todos.map(todo => {
+          if (category === todo.category) {
+            return (
+              <View style={styles.eachTodoCard} key={todo.id}>
+                <View style={styles.todoCard}><Text style={styles.todoText}>{todo.text}</Text></View>
+                <View style={styles.fuctionBox}>
+                  <TouchableOpacity onPress={() => setDone(todo.id)}>
+                    <AntDesign name="checkcircleo" size={30} color="black" />
+                  </TouchableOpacity>
+                  <Feather name="edit" size={30} color="black" />
+                  <AntDesign name="delete" size={30} color="black" />
+                </View>
+              </View>
+            );
+          }
+        })}
+
+
+
+
         <View style={styles.eachTodoCard}>
           <View style={styles.todoCard}><Text style={styles.todoText}>Todo List</Text></View>
           <View style={styles.fuctionBox}>
@@ -58,7 +107,7 @@ export default function App() {
         </View>
 
       </ScrollView>
-    </SafeAreaView>
+    </SafeAreaView >
 
 
   );
